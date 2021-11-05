cc256a117c0b16015977df908c32e4583a3de152
csa <csa.devomax@gmail.com>
2021-11-05 18:00:11 +0100

    validation firstname

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,40 +1,53 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var x = document.getElementById('myTopnav')
+  if (x.className === 'topnav') {
+    x.className += ' responsive'
   } else {
-    x.className = "topnav";
+    x.className = 'topnav'
   }
 }
+// #2 Implement form entries
+function validate() {
+  console.log('Validate')
+
+  let formValid = true
+
+  const inputFirstName = document.querySelector('#first')
+  const firstName = inputFirstName.value
+
+  if (firstName.length < 2) {
+    formValid = false
+  }
+  return formValid
+}
+
+
+
 
 /// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector('.bground')
+const modalBtn = document.querySelectorAll('.modal-btn')
+const formData = document.querySelectorAll('.formData')
 //*close modal*//
-const modalClose = document.querySelectorAll("span.close");
-
+const modalClose = document.querySelectorAll('span.close')
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal))
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = 'block'
 }
 
 // #1 TODO : close modal
 
 // Close modal event
-modalClose.forEach((close) => close.addEventListener("click", closeModal));
+modalClose.forEach((close) => close.addEventListener('click', closeModal))
 
 // close modal form
 function closeModal() {
-  modalbg.style.display = "none";
+  modalbg.style.display = 'none'
 }
 
 
-// #2 Implement form entries
 
-const form = document.getElementById('form');
-const firstName = document.getElementById ('first');
