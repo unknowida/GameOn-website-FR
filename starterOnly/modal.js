6080a3cd94ab4e4012ba6f3c4fc0f12a3b0c6234
csa <csa.devomax@gmail.com>
2021-11-08 13:10:06 +0100

    validate email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,7 +7,7 @@ function editNav() {
   }
 }
 // #2 Implement form entries
-//Firstname
+//Firstname --> Lastname --> Email -->
 function validate() {
   console.log('Validate')
 
@@ -26,12 +26,26 @@ function validate() {
   if (lastName.length < 2) {
     formValid = false
   }
+
+  const inputEmail = document.querySelector('#email')
+  const email = inputEmail.value
+
+
+
+  // https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
+  // eslint-disable-next-line no-control-regex
+  const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
+  if (regex.test(email) == false) {
+    formValid = false
+  }
+
+  // if (lastName.length < 2) {
+  //   formValid = false
+  // }
+
   return formValid
 }
-//Lastname
-
-
-
 
 /// DOM Elements
 const modalbg = document.querySelector('.bground')
@@ -57,6 +71,3 @@ modalClose.forEach((close) => close.addEventListener('click', closeModal))
 function closeModal() {
   modalbg.style.display = 'none'
 }
-
-
-
