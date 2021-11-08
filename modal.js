090863b24d60856ad9498ffe68e280597bc464c2
csa <csa.devomax@gmail.com>
2021-11-08 16:02:13 +0100

    test number

--- modal.js
+++ modal.js
@@ -7,7 +7,7 @@ function editNav() {
   }
 }
 // #2 Implement form entries
-//Firstname --> Lastname --> Email -->
+//Firstname --> Lastname --> Email --> Numbers tournaments
 function validate() {
   console.log('Validate')
 
@@ -30,8 +30,6 @@ function validate() {
   const inputEmail = document.querySelector('#email')
   const email = inputEmail.value
 
-
-
   // https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
   // eslint-disable-next-line no-control-regex
   const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
@@ -40,9 +38,12 @@ function validate() {
     formValid = false
   }
 
-  // if (lastName.length < 2) {
-  //   formValid = false
-  // }
+  const inputQuantityNumber = document.querySelector('#quantity')
+  const quantityNumber = /[0-99]+$/
+
+  if (inputQuantityNumber.value.match(quantityNumber)) {
+    formValid = false
+  }
 
   return formValid
 }
